fa29a322c69a1cb8ab0c5f5d9a47dd67d8fa3103
jadephung <jadephung.29@gmail.com>
2019-08-08 18:12:07 +0700

    finish preOrderTraversal

--- src/BinaryTreeTraversals.scala
+++ src/BinaryTreeTraversals.scala
@@ -1,6 +1,43 @@
+import scala.io.Source
+
 object BinaryTreeTraversals {
-def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
+    val source = Source.fromFile("D:\\CourseraHW\\week4_binary_search_trees\\1_tree_traversals\\tests\\21").getLines().toArray.drop(1)
+
+    //    val input = source.map(x => (x.split(" ")(0).toInt, x.split(" ")(1).toInt, x.split(" ")(2).toInt))
+
+    val input = Array((0, 7, 2, 0),
+      (10, -1, -1, 0),
+      (20, -1, 6, 0),
+      (30, 8, 9, 0),
+      (40, 3, -1, 0),
+      (50, -1, -1, 0),
+      (60, 1, -1, 0),
+      (70, 5, 4, 0),
+      (80, -1, -1, 0),
+      (90, -1, -1, 0))
+
+    val treeTraverse = new TreeTraversal
+
+    treeTraverse.preOrderTraversal(input)
 
 
-}
+    //    val output = treeTraverse.inOrderTraversal(input)
+    //    val correctOutputSource = Source.fromFile("D:\\CourseraHW\\week4_binary_search_trees\\1_tree_traversals\\tests\\21.a").getLines().toArray
+    //
+    //    val correctOutput = correctOutputSource(0).split(" ").map(_.toInt)
+    //    for (i <- 0 until correctOutput.length) {
+    //      val isTrue = if (output(i) == correctOutput(i)) {
+    //        true
+    //      } else {
+    //        false
+    //      }
+    //      println("index: " + i + " calculated output: " + output(i) + " correct output : " + correctOutput(i) + " " + isTrue)
+
+
+    //  }
+
+
+  }
+
 }
--- src/TreeTraversal.scala
+++ src/TreeTraversal.scala
@@ -1,4 +1,5 @@
 import scala.collection.mutable._
+import scala.util.control.Breaks
 
 class TreeTraversal {
 
@@ -6,31 +7,98 @@ class TreeTraversal {
   def inOrderTraversal(originalTree: Array[(Int, Int, Int)]): Array[Int] = {
     val result = new ArrayBuffer[Int]
     val stackNode = new Stack[(Int, Int, Int, Int)]
-    var i = 0
-    while (result.length <= originalTree.length) {
-      while (originalTree(i)._3 != -1) {
-        stackNode.push((i, originalTree(i)._1, originalTree(i)._2, originalTree(i)._3))
-        i = originalTree(i)._3
-      }
-      result.append(stackNode.top._2)
-      if (stackNode.top._4 == -1){
-      stackNode.pop()
-      }else {
-        i = stackNode.top._4
-      }
+    var currentIndex = 0
 
+    while (result.length < originalTree.length) {
+
+      //Check node left
+      do {
+        stackNode.push((currentIndex, originalTree(currentIndex)._1, originalTree(currentIndex)._2, originalTree(currentIndex)._3))
+        currentIndex = originalTree(currentIndex)._2
+        //        stackNode.foreach(x => println("stack traverse left: " + x))
+        //        println("value of i " + currentIndex)
+      } while (currentIndex != -1)
+
+      //print key at current position (no more left child)
+      result.append(stackNode.top._2)
+      //      println("result list after print key 1: " + result.mkString(" "))
+
+      //check if can traverse right :
+      val loopControl = new Breaks
+      loopControl.breakable {
+        while (stackNode.top._4 == -1) {
+          stackNode.pop()
+          if (stackNode.nonEmpty) {
+            stackNode.update(0, (stackNode.top._1, stackNode.top._2, -1, stackNode.top._4))
+            if (result.length <= 1 || !result.contains(stackNode.top._2)) {
+              result.append(stackNode.top._2)
+            }
+          } else {
+            loopControl.break()
+          }
+        }
+
+      }
+      //      println("result list after print key 2: " + result.mkString(" "))
+      if (stackNode.nonEmpty) {
+        currentIndex = stackNode.top._4
+        stackNode.update(0, (stackNode.top._1, stackNode.top._2, stackNode.top._3, -1))
+      }
 
     }
 
     result.toArray
   }
 
-  def preOrderTraversal(originalTree: Array[(Int, Int, Int)]): Array[Int] = {
+  def preOrderTraversal(originalTree: Array[(Int, Int, Int, Int)]): Array[Int] = {
+    val result = new ArrayBuffer[Int]
+    val stackNode = new Stack[(Int, Int, Int, Int)] //index of the node, key node, index left node, index right node
+    var currentIndex = 0
+
+    //traverse:
+    //from root down to left, if left out then right
+    //Original tree: Key, left index, right index, recorded?
+    while (result.length < originalTree.length) {
+      //traverse left-most
+      do {
+        stackNode.push((currentIndex, originalTree(currentIndex)._1, originalTree(currentIndex)._2, originalTree(currentIndex)._3))
+        result.append(originalTree(currentIndex)._1)
+        originalTree.update(currentIndex, (originalTree(currentIndex)._1, originalTree(currentIndex)._2, originalTree(currentIndex)._3, 1))
+        currentIndex = originalTree(currentIndex)._2
+      } while (currentIndex != -1)
+
+      //right traverse:
+
+      val loopControl = new Breaks
+      loopControl.breakable {
+        while (stackNode.top._4 == -1) {
+          stackNode.pop()
+          if (stackNode.nonEmpty) {
+            stackNode.update(0, (stackNode.top._1, stackNode.top._2, -1, stackNode.top._4))
+            if (originalTree(stackNode.top._1)._4 == 0) {
+              result.append(stackNode.top._2)
+            }
+          } else {
+            loopControl.break()
+          }
+        }
+
+      }
+      println("result list after print key 2: " + result.mkString(" "))
+      if (stackNode.nonEmpty) {
+        currentIndex = stackNode.top._4
+        stackNode.update(0, (stackNode.top._1, stackNode.top._2, stackNode.top._3, -1))
+      }
+
+
+    }
+    result.toArray
+
 
   }
 
   def postOrderTraversal(originalTree: Array[(Int, Int, Int)]): Array[Int] = {
-
+    null
   }
 
 
